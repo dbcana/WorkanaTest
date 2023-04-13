940ae4d7e0d56546f4a6af8e7822071f88e65b89
Daniel Caña <cana.daniel.borba@gmail.com>
2023-04-13 09:32:34 -0300

    Initial structure

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Workana Test</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="/app/assets/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Bootstrap core JS -->
+        <script src="/app/assets/js/jquery-3.3.1.slim.min.js"></script>
+        <script src="/app/assets/js/popper.min.js"></script>
+        <script src="/app/assets/js/bootstrap.min.js"></script>
+    </head>
+    <body class="vsc-initialized">
+        <div class="container">
+            <header class="blog-header py-3">
+                <nav class="nav d-flex justify-content-between" style="float: left;">
+                    <a class="p-3 text-muted" href="#">Customers</a>
+                    <a class="p-3 text-muted" href="#">Taxes</a>
+                    <a class="p-3 text-muted" href="#">Categories</a>
+                    <a class="p-3 text-muted" href="#">Products</a>
+                    <a class="p-3 text-muted" href="#">Sales</a>
+                </nav>
+            </header>
+        </div>
+    </body>
+</html>
